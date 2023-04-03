844438b2a3fad941e405d6c6465edae44f29e992
feudal95 <diegoeduardo-88@hotmail.com>
2023-04-03 06:10:29 +0000

    imagenes

---
+++ formulario.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="server2.php" method="get">
+        <label for="nombre">nombre</label>
+        <input type="text" name="nombre" id="nombre">
+
+        <label for="edad">edad</label>
+        <input type="text" name="edad" id="edad">
+        <button>mandar formulario</button>
+
+
+    </form>
+</body>
+
+</html>
