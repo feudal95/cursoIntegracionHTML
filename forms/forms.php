76a51aa9824633b159fef58762523299ec2a9638
feudal95 <diegoeduardo-88@hotmail.com>
2023-04-05 07:07:19 +0000

    seguir

--- forms/forms.php
+++ forms/forms.php
@@ -16,10 +16,32 @@
         -->
 
         <!-- Arreglos -->
-        <label for="personas">Personas:</label>
+        <!-- 
+            <label for="personas">Personas:</label>
         <input type="text" name="personas[]" id="">
         <input type="text" name="personas[]" id="">
         <input type="text" name="personas[]" id="">
+     -->
+
+        <!-- <label for="persona">Personas:</label>
+        <input type="text" name="persona[nombre]" id="">
+        <input type="email" name="persona[email]" id="">
+        <input type="number" name="persona[edad]" id=""> -->
+
+        <!-- <input type="checkbox" name="list1">
+        <input type="checkbox" name="list2" value="este valor fue clicado">
+        <input type="checkbox" name="list3"> -->
+
+        <!-- <label for="mexico"> Mexico </label>
+        <input type="radio" name="pais" value="Mexico" id="mexico">
+        <label for="colombia"> Colombia </label>
+        <input type="radio" name="pais" value="Colombia" id="colombia">
+        <label for="peru"> Peru </label>
+        <input type="radio" name="pais" value="Peru" id="peru">
+         -->
+
+        <label for="galeria"> Carga tus imagenes </label>
+        <input type="file" multiple name="galeria[]" id="galeria">
 
         <button type="submit">Mandar Formulario</button>
     </form>
--- forms/server.php
+++ forms/server.php
@@ -3,6 +3,13 @@
 echo "<pre>";
 
 //var_dump($_POST["nombre"]);
-var_dump($_POST["personas"]);
+//var_dump($_POST["personas"]);
+//var_dump($_POST["persona"]);
+//var_dump($_POST["list1"]);
+//var_dump($_POST["list2"]);
+//var_dump($_POST["list3"]);
+
+//var_dump($_POST["pais"]);
+print_r($_FILES["galeria"]);
 
 echo "</pre>";
